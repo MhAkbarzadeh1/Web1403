898b792cf24a78dcf29e9119cb887eb2b38c2162
MhAkbarzadeh1 <ghmahdieakhesam@gmail.com>
2024-10-15 17:33:49 +0330

    jalase 3

---
+++ C/C-3-index.js
@@ -0,0 +1,63 @@
+let cmd = require('./C-1-cmd.js');
+let fs = require('fs');
+
+cmd.use("minus", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) - cmd.parseInput(contInputs[2]));
+});
+cmd.use("sum", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) + cmd.parseInput(contInputs[2]));
+});
+cmd.use("multiply", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) * cmd.parseInput(contInputs[2]));
+});
+cmd.use("div", function(contInputs){
+    console.log(cmd.parseInput(contInputs[1]) / cmd.parseInput(contInputs[2]));
+});
+cmd.use("printRecord", function(contInputs){
+    console.log(({
+        name: contInputs[1],
+        family: contInputs[2],
+        age: contInputs[3],
+        email: contInputs[4]
+    }));
+});
+cmd.use("saveRecord", function(contInputs){
+    fs.writeFile('myDatabase.txt', contInputs, {encoding:'utf8'}, function(error){
+        if(error){
+            console.log('ERROR:', error);
+        }
+        else{
+            console.log('File Saved.')
+        }
+    })
+});
+
+cmd.use("saveRecord2", function(contInputs){
+    let x = {
+        name: contInputs[1],
+        family: contInputs[2],
+        age: contInputs[3],
+        email: contInputs[4]
+    };
+    x= JSON.stringify(x)
+    fs.writeFile('myDatabase.txt', x, {encoding:'utf8'}, function(error){
+        if(error){
+            console.log('ERROR:', error);
+        }
+        else{
+            console.log('File Saved.')
+        }
+    })
+});
+cmd.use("openfile", function(contInputs){
+    fs.readFile(contInputs[1], function(error,data){
+        if(error){
+            console.log('ERROR:', error);
+        }
+        else{
+            console.log('File Opened',data.toString())
+        }
+    })
+});
+
+cmd.start();
