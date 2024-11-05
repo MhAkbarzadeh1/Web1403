ab3daf582e86bab8fcbd327d9a9e224b9a479d9e
MhAkbarzadeh1 <ghmahdieakhesam@gmail.com>
2024-11-05 18:16:09 +0330

    UP

---
+++ F/F-app-3.js
@@ -0,0 +1,81 @@
+const path = require('path');
+let app = require('./F-http-1.js');
+let fs = require('fs');
+
+function write(response, body){
+    response.write(JSON.stringify(body));
+    response.end();
+}
+
+app.use('/sum', function(request, response){
+    result = {
+        data: parseInt(request.path[2]) + parseInt(request.path[3])
+    };
+    write(response, result);
+});
+
+app.use('/multiply', function(request, response){
+    result = {
+        data: parseInt(request.path[2]) * parseInt(request.path[3])
+    };
+    write(response, result);
+});
+
+app.use('/printRecord', function(request, response){
+    result = {
+        "name": request.path[2],
+        "family": request.path[3],
+        "email": request.path[4]
+    }
+    write(response, result);
+});
+
+app.use('/saveRecord', function(request, response){
+    result = {
+        "name": request.path[2],
+        "family": request.path[3],
+        "email": request.path[4]
+    }
+    writeFile('database.txt', x, function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved. sdfdsfds'});
+    });
+});
+
+app.use('/create', function(request, response){
+    result = {
+        "name": request.path[2],
+        "family": request.path[3],
+        "email": request.path[4]
+    }
+    writeFile(request.path[2], x, function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved.'});
+    });
+});
+app.use('/file', function(request, response){
+    result = {
+        "name": request.data[0],
+        "content": request.data[1]
+        }
+        
+    writeFile(request.data.body, request.data.content, function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved.'});
+    });
+});
+
+function writeFile(name, body, callback){
+    body = JSON.stringify(body);
+    fs.writeFile(name, body, function(error){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, { result: 'ERROR:', error});
+        }
+        else{
+            callback();
+        }
+    })
+}
+
+app.start();
---
+++ F/F-http-1.js
@@ -0,0 +1,39 @@
+let http = require('http');
+let controllers = [];
+
+function use(url, func){
+    let x = {
+        url: url,
+        function: func
+    };
+    controllers.push(x);
+}
+
+function route(request, response){
+    let found = false;
+    for(let item of controllers){
+        //if(item.url === request.url){
+        if(request.url.startsWith(item.url)){
+            item.function(request, response);
+            found = true;
+        }
+    }
+    if(!found){
+        console.log('Path not found.');
+    }
+}
+
+function start(){
+    console.log('start')
+    let server = http.createServer(function(request, response){
+        request.path =  request.url.split('/')
+        console.log(request.method, request.url);
+        route(request, response);
+    });
+    server.listen(80);
+}
+
+module.exports = {
+    use: use,
+    start: start
+}
